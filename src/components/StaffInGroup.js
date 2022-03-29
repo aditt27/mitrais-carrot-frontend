68dfe70e0fdbd1198f67df4bb0b2b181a7df8dba
arkaanni <arkaannuruliman@gmail.com>
2022-03-29 15:07:05 +0700

    add pagination to staff in group list

--- src/components/StaffInGroup.js
+++ src/components/StaffInGroup.js
@@ -1,4 +1,4 @@
-import { Tab, Container, Row, Col, Form, Table } from 'react-bootstrap';
+import { Container, Row, Col, Form, Table, Pagination } from 'react-bootstrap';
 import { Component } from 'react';
 import axios from 'axios';
 
@@ -8,7 +8,10 @@ class StaffInGroup extends Component {
         super()
         this.state = {
             groupList: [],
-            staffList: []
+            staffList: [],
+            currentPageStaff: 0,
+            totalPageStaff: 0,
+            currentGroup: 'no_group'
         }
     }
     
@@ -21,22 +24,26 @@ class StaffInGroup extends Component {
             }
         })
 
-        const initialStaff = await this.filterStaffByGroup()
+        const { staffList, totalPages } = await this.filterStaffByGroup()
 
-        this.setState({groupList: groups, staffList:  initialStaff})
+        this.setState({groupList: groups, staffList:  staffList, totalPageStaff: totalPages})
     }
 
-    async filterStaffByGroup(groupName = "no_group") {
-        let staff = []
+    async filterStaffByGroup(groupName = "no_group", page = 0) {
+        let result = {
+            staffList: [],
+            currentPage: 0
+        }
+        
         const filter = groupName === "no_group" ? "filterBy=no_group" : `filterBy=group&filterValue=${groupName}`
-        await axios.get(`http://localhost:8081/api/v1/user?${filter}`).then(res => {
+        await axios.get(`http://localhost:8081/api/v1/user?${filter}&page=${page}`).then(res => {
             const data = res.data.result
             if (data.currentPageContent) {
                 const st = data.currentPageContent.filter(s => s.role === "Staff")
                 let i = 1
                 if (st.length > 0) {
                     st.forEach((s) =>
-                        staff.push({
+                        result.staffList.push({
                             no: i++,
                             username: s.username,
                             name: s.name,
@@ -45,15 +52,24 @@ class StaffInGroup extends Component {
                         })
                     )
                 }
+                result.currentPage = data.currentPage
+                result.totalPages = data.totalPages
             }
-        })
-        return staff
+        }).catch(e => {})
+        return result
     }
 
     handleGroupSelect = async (e) => {
         const groupName = e.target.value
-        const staff = await this.filterStaffByGroup(groupName)
-        this.setState({staffList: staff})
+        const { staffList, currentPage } = await this.filterStaffByGroup(groupName)
+        this.setState({staffList: staffList, currentPageStaff: currentPage, currentGroup: groupName})
+    }
+
+    fetchStaff = (page) => {
+        this.filterStaffByGroup(this.state.currentGroup, page).then(res => {
+            const { currentPage, staffList } = res
+            this.setState({staffList: staffList, currentPageStaff: currentPage})
+        }).catch(e => {})
     }
 
     GroupListOptions = () => {
@@ -76,59 +92,72 @@ class StaffInGroup extends Component {
         })
     }
 
-    render() {
+    PaginationItems = () => {
+        const { totalPageStaff, currentPageStaff } = this.state
+        let paginationItems = []
+        for (let i = 1; i <= totalPageStaff; i++) {
+            paginationItems.push(
+                <Pagination.Item key={i} active={i === currentPageStaff + 1} activeLabel="" onClick={() => this.fetchStaff(i - 1)}>{i}</Pagination.Item>
+            )
+        }
+        return paginationItems
+    }
 
+    render() {
         return (
             <Container className="px-4">
-                <Tab.Content>
-                    <Row>
-                        <Col md={12} className="align-self-start my-2">
-                            <hr style={{
-                                width: "2em",
-                                backgroundColor: "orange",
-                                height: "0.2em"
-                            }} align="left"/>
-                            <h4 className="box-title">STAFF IN GROUP LIST</h4>
-                        </Col>
-                        <Col md="12" className="my-2">
-                            <Form.Group className="float-right">
-                                <Form.Label>STAFF GROUP</Form.Label>
-                                <Form.Control id="group-filter" as="select" defaultValue="no_group" className="mx-auto" onChange={this.handleGroupSelect}>
-                                    <option value="no_group">No Group</option>
-                                    <this.GroupListOptions />
-                                </Form.Control>
+                <Row>
+                    <Col md={12} className="align-self-start my-2">
+                        <hr style={{
+                            width: "2em",
+                            backgroundColor: "orange",
+                            height: "0.2em"
+                        }} align="left"/>
+                        <h4 className="box-title">STAFF IN GROUP LIST</h4>
+                    </Col>
+                    <Col md="12" className="my-2">
+                        <Form.Group className="float-right">
+                            <Form.Label>STAFF GROUP</Form.Label>
+                            <Form.Control id="group-filter" as="select" defaultValue="no_group" className="mx-auto" onChange={this.handleGroupSelect}>
+                                <option value="no_group">No Group</option>
+                                <this.GroupListOptions />
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="12" className="my-2">
+                    <Form>
+                        <Form.Group className="float-right form-inline">
+                            <Form.Label>SEARCH: &nbsp;</Form.Label>
+                                <Form.Control type="text" className="w-auto" />
                             </Form.Group>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md="12" className="my-2">
-                            <Form>
-                                <Form.Group className="float-right form-inline">
-                                    <Form.Label>SEARCH: &nbsp;</Form.Label>
-                                    <Form.Control type="text" className="w-auto" />
-                                </Form.Group>
-                            </Form>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md="12" className="my-2">
-                            <Table striped bordered hover>
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Username</th>
-                                        <th>Name</th>
-                                        <th>Role</th>
-                                        <th>Office</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <this.StaffListRow />
-                                </tbody>
-                            </Table>
-                        </Col>
-                    </Row>
-                </Tab.Content>
+                        </Form>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="12" className="my-2">
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Username</th>
+                                    <th>Name</th>
+                                    <th>Role</th>
+                                    <th>Office</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <this.StaffListRow />
+                            </tbody>
+                        </Table>
+                    </Col>
+                    <Col md="12">
+                        <Pagination className="float-right">
+                            <this.PaginationItems />
+                        </Pagination>
+                    </Col>
+                </Row>
             </Container>
         )
     }
