6c52e28a44e9a7833e8eb916cb15ae0d935c2d46
Aditya Budi Laksono <adityabudi.laksono@mitrais.com>
2022-03-29 20:47:36 +0700

    update bazaa item image

--- src/components/BazaarItemCard.js
+++ src/components/BazaarItemCard.js
@@ -5,7 +5,7 @@ class BazaarItemCard extends React.Component {
     render() {
         return (
             <Card style={{ width: '250px', margin: '4px' }}>
-                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                <Card.Img variant="top" src={this.props.image} />
                 <Card.Body>
                     <Card.Title>{this.props.title}</Card.Title>
                     <Card.Text>{this.props.price} Carrots</Card.Text>
