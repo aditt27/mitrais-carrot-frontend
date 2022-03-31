0877ea32bdb11e54a1455fb872292b540736a75e
ZainulSaifulah <zainulsaifulahaziz@gmail.com>
2022-03-31 10:52:04 +0700

    add edit parameter

--- src/apis/ParameterApi.js
+++ src/apis/ParameterApi.js
@@ -11,3 +11,15 @@ export async function getParameters() {
         }))
         .catch(err => console.log(err))
 }
+
+export async function editParameter(parameter) {
+  return apiClient
+      .put(`/parameter/${parameter.id}`, parameter)
+      .then((response=> {
+          if(response) {        
+              return response.data
+          }
+          return false
+      }))
+      .catch(err => console.log(err))
+}
