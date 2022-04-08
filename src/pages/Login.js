010ffebce1487d60e908819faa26f154268d90e3
ZainulSaifulah <zainulsaifulahaziz@gmail.com>
2022-04-08 08:10:14 +0700

    add login validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
 import MCarrotLogo from '../assets/img/mitrais-logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Button, Card, Form } from 'react-bootstrap'
-import { useDispatch } from 'react-redux';
+import { Button, Card, Form, Alert } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../stores/authThunk';
-import  { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState('');
+  const [validated, setValidated] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const rejected = useSelector(state => state.authReducer.rejected);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(login({username, password, navigate}));
+    const form = e.currentTarget;
+
+    if (form.checkValidity() === true) {
+      e.preventDefault();
+      e.stopPropagation();
+      dispatch(login({ username, password, navigate }));
+    } else {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
   }
 
   return (
@@ -27,17 +39,26 @@ export default function Login() {
                 <img src={MCarrotLogo}
                   alt='Mitrais Carrot Logo' className="img-fluid" />
               </div>
-              <Form className="form-signin" method="POST" onSubmit={handleSubmit}>
+              <Form className="form-signin" method='POST' noValidate validated={validated} onSubmit={handleSubmit}>
+                <Alert show={rejected} variant="danger">
+                  username or password incorrect
+                </Alert>
                 <Form.Group className="mb-3 form-label-group">
-                  <Form.Control type="text" id="username" name="username" placeholder="Username" required="" autoFocus="" onChange={(e) => setUsername(e.target.value)} />
+                  <Form.Control type="text" id="username" name="username" placeholder="Username" required onChange={(e) => setUsername(e.target.value)} />
+                  <Form.Control.Feedback type="invalid">
+                    Username cannot blank
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3 form-label-group">
-                  <Form.Control type="password" id="password" name="password" placeholder="Password" required="" onChange={(e) => setPassword(e.target.value)} />
+                  <Form.Control type="password" id="password" name="password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)} />
+                  <Form.Control.Feedback type="invalid">
+                    Password cannot blank
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Button className="btn-block btn-mitrais text-uppercase" type="submit">
-                  <FontAwesomeIcon icon="lock"  className='mr-2'/>
+                  <FontAwesomeIcon icon="lock" className='mr-2' />
                   Sign in
                 </Button>
               </Form>
