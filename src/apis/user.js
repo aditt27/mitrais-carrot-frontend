6831d47c501dff476399bf07b71ab8186e042f2d
arkaanni <arkaannuruliman@gmail.com>
2022-04-14 11:26:35 +0700

    get total earned carrot

--- src/apis/user.js
+++ src/apis/user.js
@@ -117,3 +117,7 @@ export async function totalSpentForBazaar(userId) {
         })
         .catch(err => console.log(err))
 }
+
+export async function getTotalEarnedCarrot(userId) {
+    return await apiClient.get(`/user/total_earn/${userId}`).then(res => res.data)
+}
